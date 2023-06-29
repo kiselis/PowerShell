f097a25e8e2fe07a1ca522efe27b100bd1bfcb1f
Jonas Kiselis <31988446+kiselis@users.noreply.github.com>
2023-06-29 19:44:53 +0100

    Find biggest fils
    This script should allow you to find the biggest files in the selected folder and all its subfolders

---
+++ find_biggest_files.ps1
@@ -0,0 +1,15 @@
+# This script should find top biggest files in specified folder and its subfolders
+
+# Specify Path
+$FolderPath = "C:\Users\kisej"
+
+# Fetches the list of all files in the folder and subfolders
+$Files = Get-ChildItem -Path $FolderPath -Recurse -File
+
+# Finds 5 biggest files by size
+$TopFiles = $Files | Sort-Object -Property Length -Descending | Select-Object -First 5
+
+# Prints out the result
+$TopFiles | ForEach-Object {
+    Write-Output "File: $($_.FullName), Size: $($_.Length) bytes"
+}
