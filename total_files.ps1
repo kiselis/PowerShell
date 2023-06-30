2e556790f876b5109887d5d55153459f69cc4593
Jonas Kiselis <31988446+kiselis@users.noreply.github.com>
2023-06-30 22:22:04 +0100

    Finds total number of filed in disk C
    It will take a few minutes to return the result.

---
+++ total_files.ps1
@@ -0,0 +1,12 @@
+# Finds the total number of files in disk C
+# It may take a few minutes to get the total file count
+
+$FolderPath = "C:\"
+
+$Files = Get-ChildItem -Path $FolderPath -File -Recurse -ErrorAction SilentlyContinue
+if ($Files) {
+    $FileCount = $Files.Count
+    Write-Host "Total number of files: $FileCount"
+} else {
+    Write-Host "File count failed"
+}
